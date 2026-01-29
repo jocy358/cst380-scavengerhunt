eea16cf824b6d7d696a12a1f63d06c1ff1957e17
jocy <lavenderviolets228@gmail.com>
2026-01-29 14:35:45 -0800

    Initial Commit

---
+++ 380-scavengerhunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  380-scavengerhunt
+//
+//  Created by Jocy Cortez on 1/29/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
